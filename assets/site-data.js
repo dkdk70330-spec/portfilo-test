1ba91bdc7cb45a19fbfb2f0e51218c11efc4ec4b
dkdk70330-spec <dkdk70330@gmail.com>
2026-07-24 00:56:08 +0900

    Add files via upload

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,166 +1,82 @@
 window.PORTFOLIO_PROJECT = {
   "version": 1,
   "site": {
-    "title": "월하",
-    "description": "변태놈",
-    "textColor": "#a98737",
-    "themeColor": "#836189"
+    "title": "콘보따리",
+    "description": "",
+    "textColor": "#f4f1ea",
+    "themeColor": "#a9c5fe"
   },
   "creator": {
     "avatar": "./assets/images/creator-avatar.png",
     "fallbackText": "",
-    "name": "월하",
-    "handle": "@moon-pervert",
-    "bio": [
-      "변태같은 걸 좋아해요 우따따 젖병물려주세요"
-    ],
+    "name": "푸딩냥",
+    "handle": "@puddingcat",
+    "bio": [],
     "links": [
       {
         "id": "x",
-        "url": "https://dkdk70330-spec.github.io/portfolio/#characters"
-      },
-      {
-        "id": "insta",
-        "url": "https://dkdk70330-spec.github.io/portfolio/#characters"
-      },
-      {
-        "id": "lover",
-        "url": "https://dkdk70330-spec.github.io/portfolio/#characters"
-      },
-      {
-        "id": "slash",
-        "url": "https://dkdk70330-spec.github.io/portfolio-generator/generator/"
+        "url": "https://x.com/6zerry9"
       }
     ],
     "background": "./assets/images/creator-background.png"
   },
   "worlds": [
     {
-      "id": "world-8f195339-1a84-4a57-a631-7d4d16936466",
-      "name": "아르키아 제국",
-      "subtitle": "어쩌고 저쩌고 연대기",
-      "image": "./assets/images/world-world-8f195339-1a84-4a57-a631-7d4d16936466.png",
+      "id": "world-c06ffe80-4ad9-4b4c-989c-7654784f4825",
+      "name": "아르테아",
+      "subtitle": "",
+      "image": "./assets/images/world-world-c06ffe80-4ad9-4b4c-989c-7654784f4825.png",
       "tags": [
-        "제국",
-        "판타지",
-        "전생"
+        "판타지"
       ],
-      "description": [
-        "어쩌고 저쩌고 저쩌고 변경사항"
-      ],
-      "sections": [
-        {
-          "id": "world-section-4a638c4e-b620-4bde-b511-3bec0270b210",
-          "title": "문화어쩌고",
-          "content": [
-            "ㅋㅋ"
-          ],
-          "collapsible": true
-        },
-        {
-          "id": "world-section-c0cef866-5e39-4bd4-bbe2-4659e59b9b27",
-          "title": "비설이랑 접기 기능이 없나?",
-          "content": [],
-          "collapsible": true
-        }
-      ],
+      "description": [],
+      "sections": [],
       "music": []
     }
   ],
   "characters": [
     {
-      "id": "character-01337a5a-5f6b-4a8a-886f-9f511fb0cf91",
-      "worldId": "world-8f195339-1a84-4a57-a631-7d4d16936466",
-      "name": "접점",
-      "subtitle": "상호 침범의 조건",
+      "id": "character-e8e842ad-1f1a-4854-af14-f63657cd7e2d",
+      "worldId": "world-c06ffe80-4ad9-4b4c-989c-7654784f4825",
+      "name": "마물전생",
+      "subtitle": "알에서 시작하는 진화의 길",
       "description": [
-        "어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵어쩌고 저쩌고 이러쿵 저러쿵"
+        "어쩌고 저쩌고 뭐라도 되어보자"
       ],
       "genres": [],
       "tags": [
-        "어쩌고",
-        "저쩌고",
-        "우따따"
+        "판타지",
+        "이세계"
       ],
-      "featured": true,
+      "featured": false,
       "images": [
-        "./assets/images/character-character-01337a5a-5f6b-4a8a-886f-9f511fb0cf91-1.png"
+        "./assets/images/character-character-e8e842ad-1f1a-4854-af14-f63657cd7e2d-1.png"
       ],
       "platforms": [
         {
-          "id": "tingle",
-          "url": ""
-        },
-        {
-          "id": "rofan",
-          "url": ""
-        },
-        {
-          "id": "caveduck",
-          "url": ""
-        },
-        {
           "id": "bloom",
-          "url": ""
-        },
-        {
-          "id": "tpat",
-          "url": ""
+          "url": "https://bloom-ai.me/content/7779a492-d4a9-4f52-9b4f-9b59431d1a10"
         }
       ],
       "contents": [
         {
-          "id": "character-content-690eb937-db47-4dde-bdce-cb4007b947da",
-          "type": "비하인드",
-          "title": "어쩌고하이",
+          "id": "character-content-762b51a6-9aa2-47b6-a990-ff5416378614",
+          "type": "제작 비하인드",
+          "title": "회빙환",
           "content": [
-            "비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀"
+            "어쩌고저쩌고"
           ],
           "spoiler": true,
-          "warning": ""
-        },
-        {
-          "id": "character-content-fd1aa5ab-a0f1-48e8-a432-d320118813ca",
-          "type": "비밀이야기 비밀",
-          "title": "비밀이야기 비밀",
-          "content": [
-            "비밀이야기 비밀"
-          ],
-          "spoiler": true,
-          "warning": ""
-        },
-        {
-          "id": "character-content-1e153161-8937-432f-81c3-579e70f9e397",
-          "type": "비밀이야기 비밀",
-          "title": "비밀이야기 비밀비밀이야기 비밀",
-          "content": [
-            "비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀비밀이야기 비밀"
-          ],
-          "spoiler": false,
-          "warning": ""
+          "warning": "누르면 죽소"
         }
       ],
       "music": [
         {
-          "id": "music-8d4268ec-23d6-499d-99ed-0e31f658662e",
-          "title": "노래",
-          "type": "youtube",
-          "url": "https://www.youtube.com/watch?v=d0LmGh5Wwrs&list=RDd0LmGh5Wwrs&start_radio=1",
-          "file": ""
-        },
-        {
-          "id": "music-1f33adca-98cb-44cc-882b-06759b6bdd79",
-          "title": "ㅇㄴㅁㅇ",
-          "type": "youtube",
-          "url": "https://www.youtube.com/watch?v=d0LmGh5Wwrs&list=RDd0LmGh5Wwrs&start_radio=1",
-          "file": ""
-        },
-        {
-          "id": "music-894d0035-b522-4818-97ba-bc4830c9b679",
+          "id": "music-95e712b6-d7ec-43ca-b81d-c825e2ed00dc",
           "title": "부재",
           "type": "mp3",
           "url": "",
-          "file": "./assets/audio/character-character-01337a5a-5f6b-4a8a-886f-9f511fb0cf91-music-894d0035-b522-4818-97ba-bc4830c9b679.mp3"
+          "file": "./assets/audio/character-character-e8e842ad-1f1a-4854-af14-f63657cd7e2d-music-95e712b6-d7ec-43ca-b81d-c825e2ed00dc.mp3"
         }
       ]
     }
@@ -173,21 +89,6 @@ window.PORTFOLIO_CATALOG = {
       "id": "x",
       "name": "트위터",
       "icon": "./assets/catalog/profile-links/x.png"
-    },
-    {
-      "id": "insta",
-      "name": "인스타",
-      "icon": "./assets/catalog/profile-links/insta.png"
-    },
-    {
-      "id": "slash",
-      "name": "슬래시",
-      "icon": "./assets/catalog/profile-links/slash.png"
-    },
-    {
-      "id": "lover",
-      "name": "러버미",
-      "icon": "./assets/catalog/profile-links/lover.png"
     }
   ],
   "platforms": [
@@ -195,26 +96,6 @@ window.PORTFOLIO_CATALOG = {
       "id": "bloom",
       "name": "블룸",
       "icon": "./assets/catalog/platforms/bloom.png"
-    },
-    {
-      "id": "caveduck",
-      "name": "케덕",
-      "icon": "./assets/catalog/platforms/caveduck.png"
-    },
-    {
-      "id": "rofan",
-      "name": "로판",
-      "icon": "./assets/catalog/platforms/rofan.png"
-    },
-    {
-      "id": "tpat",
-      "name": "티팟",
-      "icon": "./assets/catalog/platforms/tpat.png"
-    },
-    {
-      "id": "tingle",
-      "name": "팅글",
-      "icon": "./assets/catalog/platforms/tingle.png"
     }
   ],
   "genres": [
